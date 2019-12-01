5d91f33b2c1f172112f8f91f974e46a6f5bb1c0f
darkTeiman <Alex.Teiman@yandex.by>
2019-12-01 15:11:31 +0300

    added slider followers

--- app/js/main.js
+++ app/js/main.js
@@ -13,11 +13,23 @@ $(function () {
    arrows:false,
     
   });
+   $(".followers__inner").slick({
+     arrows: false,
+     slidesToShow: 3,
+    slidesToScroll: 3
+
+   });
   var mixer = mixitup('.products__inner-box');
   $('.arrows-left').on('click', function () {
       $('.slider__inner').slick('slickPrev');
     });
     $('.arrows-right').on('click', function () {
       $('.slider__inner').slick('slickNext');
+    });
+  $('.followers__slider-left').on('click', function () {
+      $('.followers__inner').slick('slickPrev');
+    });
+    $('.followers__slider-right').on('click', function () {
+      $('.followers__inner').slick('slickNext');
     });
 });
