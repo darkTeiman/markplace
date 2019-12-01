be2ee8dfd043d25754999e3b42b2fd3ea4164b4a
darkTeiman <Alex.Teiman@yandex.by>
2019-12-01 14:54:41 +0300

    added products

--- app/js/main.js
+++ app/js/main.js
@@ -2,18 +2,18 @@ $(function () {
 
   $(".rate-star").rateYo({
     rating: 4,
-    starWidth: "17px",
+    starWidth: "15px",
     ratedFill: "#ffc000",
     readOnly: true
   });
 
  
+
   $(".slider__inner").slick({
    arrows:false,
     
-
-
   });
+  var mixer = mixitup('.products__inner-box');
   $('.arrows-left').on('click', function () {
       $('.slider__inner').slick('slickPrev');
     });
